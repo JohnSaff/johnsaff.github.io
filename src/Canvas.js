e999361a749ca4c76d59fe761010adb016a1815a
John Safrany <johnsaf@fb.com>
2023-06-26 23:57:24 +0100

    removed spotify

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, createRef, useCallback } from "react";
+import React, { useRef, useState, createRef, useCallback } from "react";
 import Controls from "./Controls";
 import {
   useInterval,
@@ -9,20 +9,10 @@ const Canvas = (props) => {
   const canvasRef = useRef(null);
   const [oscillator, setOscillator] = useState(0);
   const [controlsRefArray, setControlsRefArray] = useState([]);
-  // const [isSignedIn, setIsSignedIn] = useState(false);
-  // const [accessToken, setAccessToken] = useState("");
   const [expiresIn, setExpiresIn] = useState(-1);
-  const [error, setError] = useState(null);
-  // const [nowPlaying, setNowPlaying] = useState("");
-  // const [nowPlayingTime, setNowPlayingTime] = useState(-1);
-  // const [songData, setSongData] = useState();
-  // const [code,setCode] = useState('')
 
   useInterval(() => {
-    // if (expiresIn == 0) {
-    //   refreshSpotify();
-    // }
-    if(Math.floor(oscillator)%16 == 0){
+    if(Math.floor(oscillator)%16 === 0){
       clearRect(canvasRef.current.getContext("2d"));
       controlsRefArray.forEach((element) => {
         element.current.draw(oscillator/1000);
@@ -45,13 +35,6 @@ const Canvas = (props) => {
       {controlsRefArray.map((ref, idx) => (
         <Controls key={idx} canvasRef={canvasRef} ref={ref} />
       ))}
-      {/* {isSignedIn ? (
-        <button onClick={spotifySignOut}>sign out of spotify</button>
-      ) : (
-        <button onClick={spotifySignInPKCE}> sign in with spotify</button>
-      )} */}
-      {!!error && <p>{error}</p>}
-      {/* <button onClick={getAccessToken}> refresh spotify</button> */}
     </>
   );
 };
